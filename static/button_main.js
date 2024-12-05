987855659116b7f3a46f3baad500f839ad6b5b1c
GomSon-E <gomsonixx@gmail.com>
2024-12-05 16:38:59 +0900

    FEAT : html - js 연결 테스트

---
+++ static/button_main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    // 섹션 버튼 클릭
+    $('#position_btn_container button').click(function() {
+        var value = $(this).val();
+        
+        console.log(value)
+    });
+
+});
