8ef9ff73fe63c7822339551685deea7eec33776e
GomSon-E <gomsonixx@gmail.com>
2024-12-11 17:18:03 +0900

    FEAT : 검색 결과 표시 디자인

--- static/search_main.js
+++ static/search_main.js
@@ -9,23 +9,39 @@ $(document).ready(function() {
             method: 'GET',
             data: { query: searchTerm }, // 쿼리 파라미터로 검색어 전송
             success: function(response) {
-                $('.search-container__result').empty(); // 이전 결과 비우기
+                $('.result-container').empty(); // 이전 결과 비우기
 
                 // 결과가 있을 경우
                 if (Object.keys(response).length > 0) {
-                    $('.search-container__result').append('<div>선반: ' + response.section_name + '</div>');
-                    $('.search-container__result').append('<div>이름: ' + response.item_name + '</div>');
-                    $('.search-container__result').append('<div>위치: ' + response.position + '</div>');
-                    $('.search-container__result').append('<button class="search-container__start-guide-button">안내 시작</button>');
+                    // 테이블 생성
+                    let table = `
+                        <table class="result-container__table">
+                            <thead>
+                                <tr>
+                                    <th>선반</th>
+                                    <th>이름</th>
+                                    <th>위치</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>${response.section_name}</td>
+                                    <td>${response.item_name}</td>
+                                    <td>${response.position}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button class="result-container__start-guide-button">안내<br>시작</button>`;
+                    $('.result-container').append(table);
                 } 
                 // 결과가 없을 경우 메시지 표시
                 else {
-                    $('.search-container__result').append('<div>검색결과가 없습니다.</div>');
+                    $('.result-container').append('<div class="result-container__message">검색결과가 없습니다.</div>');
                 }
             },
             error: function() {
                 // 에러 처리
-                $('.search-container__result').empty().append('<div>오류가 발생했습니다.</div>');
+                $('.result-container').empty().append('<div class="result-container__message">오류가 발생했습니다.</div>');
             }
         });
     });
