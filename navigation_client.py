3abf9feaa1bf3d79ad14ad71e8fd28a312d309e2
GomSon-E <gomsonixx@gmail.com>
2024-12-13 16:03:37 +0900

    FEAT : 주행 상태값 업데이트

--- navigation_client.py
+++ navigation_client.py
@@ -6,6 +6,7 @@ class NavigationClient:
     def __init__(self):
         self.node = rclpy.create_node('navigation_client')
         self.action_client = ActionClient(self.node, NavigateToPose, 'navigate_to_pose')
+        self.navigation_status = 0 # 0 : 주행 전 / 1 : 주행 중 / 2 : 주행 성공 / 3 : 주행 실패
 
     def send_goal(self, x, y, z, w):
         goal_msg = NavigateToPose.Goal()
@@ -32,17 +33,40 @@ class NavigationClient:
             return
         
         self.node.get_logger().info('Goal accepted!')
+        self.navigation_status = 1
 
-        # 목표 상태 피드백 수신
-        self.action_client.get_result_async(goal_handle).add_done_callback(self.get_result_callback)
+        # 목표 상태 수신
+        self.node.create_timer(0.5, lambda: self.check_result(goal_handle))
+
+    def check_result(self, goal_handle):
+        future = goal_handle.get_result_async()
+        future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         result = future.result().result
-        if result:
-            self.node.get_logger().info('Navigation completed successfully!')
-        else:
-            self.node.get_logger().error('Navigation failed!')
+        if self.navigation_status == 1:  # 한 번만 출력
+            if result:
+                self.node.get_logger().info('Navigation completed successfully!')
+                self.navigation_status = 2
+            else:
+                self.node.get_logger().error('Navigation f1ailed!')
+                self.navigation_status = 3
+            
+            # 네비게이션 클라이언트 종료
+            self.destroy()
 
     def destroy(self):
         self.node.destroy_node()
         rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    navigation_client = NavigationClient()
+
+    navigation_client.send_goal(0.257, 0.572, 0.0, 1.0)
+
+    # 주기적으로 spin()을 호출하여 콜백을 처리
+    rclpy.spin(navigation_client.node)
+
+if __name__ == '__main__':
+    main()
