a6ef9f12e49017e64603f547c99fc9c260a31475
GomSon-E <gomsonixx@gmail.com>
2024-12-23 12:17:05 +0900

    FEAT : 사용자 추적 불가 시 TTS 알림

--- base_BasicNavigator.py
+++ base_BasicNavigator.py
@@ -2,12 +2,12 @@ from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 import rclpy
 from rclpy.node import Node
+from example_interfaces.srv import AddTwoInts
 from std_msgs.msg import Bool
 from rclpy.qos import QoSProfile, HistoryPolicy, ReliabilityPolicy, DurabilityPolicy
 from rclpy.executors import MultiThreadedExecutor
 import queue
 from std_srvs.srv import Trigger
-
 class DetectionSubscriber(Node):
     def __init__(self, status_queue):
         super().__init__('detection_subscriber')
@@ -23,38 +23,32 @@ class DetectionSubscriber(Node):
             self.callback_sub,
             qos_profile
         )
-        self.status_queue = status_queue  
-
+        self.status_queue = status_queue
     def callback_sub(self, msg):
         while not self.status_queue.empty():
             self.status_queue.get_nowait()
         #self.get_logger().info(f"Received status: {msg.data}")
         self.status_queue.put(msg.data)
-
-
 class NavigationNode(Node):
     def __init__(self, status_queue):
         super().__init__('navigation_node')
-        self.status_queue = status_queue  
+        self.status_queue = status_queue
         self.navigator = BasicNavigator()
         self.navigation_status = 0
         self.navigating = False
         self.previous_status = False
         self.false_count = 0
-        self.goal_pose = None  
+        self.goal_pose = None
+        self.client = self.create_client(AddTwoInts, '/tts_status')
         self.timer = self.create_timer(0.5, self.navigate_to_goal)
-
     def navigate_to_goal(self):
         print('sdlkfhsklflsjfdljsd')
-
-        if not self.goal_pose:  
+        if not self.goal_pose:
             return
-
         try:
             status = self.status_queue.get_nowait()
         except queue.Empty:
             status = self.previous_status
-
         if status != self.previous_status:
             if status and not self.navigating:
                 self.get_logger().info("주행 시작")
@@ -66,21 +60,19 @@ class NavigationNode(Node):
                 self.navigator.cancelTask()
                 self.navigating = False
             self.previous_status = status
-
         if not status:
             self.false_count += 1
             if self.false_count >= 40:
                 self.get_logger().info("안내 문구 시작")
+                self.send_request(6,0)
                 self.false_count = 0
         else:
             self.false_count = 0
-
         if self.navigating and self.navigator.isTaskComplete():
             result = self.navigator.getResult()
             if result == TaskResult.SUCCEEDED:
                 self.navigation_status = 2
                 self.get_logger().info("Goal succeeded!")
-
                 self.timer.destroy()
                 raise RuntimeError
             elif result == TaskResult.CANCELED:
@@ -90,7 +82,6 @@ class NavigationNode(Node):
                 self.navigation_status = 3
                 self.get_logger().info("Goal failed!")
             self.navigating = False
-
     def create_goal_pose(self, x, y, z, w):
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
@@ -100,14 +91,19 @@ class NavigationNode(Node):
         goal_pose.pose.orientation.z = z
         goal_pose.pose.orientation.w = w
         return goal_pose
-
+    def send_request(self, a, b):
+        request = AddTwoInts.Request()
+        request.a = a
+        request.b = b
+        future = self.client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
 class ROS2ServiceClient(Node):
     def __init__(self):
         super().__init__('ros2_service_client')
         self.client = self.create_client(Trigger, 'home')
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for service...')
-
     def call_service(self):
         request = Trigger.Request()
         future = self.client.call_async(request)
